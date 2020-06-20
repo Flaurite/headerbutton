561769bd0322c2add046d0c6984d6f37122f14a1
Roman Pinyazhin <pinyazhin@haulmont.com>
2020-06-20 13:42:21 +0400

    Initial commit

---
+++ modules/core/test/com/flaurite/addon/headerbutton/HeaderbuttonTestContainer.java
@@ -0,0 +1,58 @@
+package com.flaurite.addon.headerbutton;
+
+import com.haulmont.cuba.testsupport.TestContainer;
+import org.junit.jupiter.api.extension.ExtensionContext;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class HeaderbuttonTestContainer extends TestContainer {
+
+    public HeaderbuttonTestContainer() {
+        super();
+        //noinspection ArraysAsListWithZeroOrOneArgument
+        appComponents = new ArrayList<>(Arrays.asList(
+                // list add-ons here: "com.haulmont.reports", "com.haulmont.addon.bproc", etc.
+                "com.haulmont.cuba"
+        ));
+        appPropertiesFiles = Arrays.asList(
+                // List the files defined in your web.xml
+                // in appPropertiesConfig context parameter of the core module
+                "com/flaurite/addon/headerbutton/app.properties",
+                // Add this file which is located in CUBA and defines some properties
+                // specifically for test environment. You can replace it with your own
+                // or add another one in the end.
+                "com/flaurite/addon/headerbutton/test-app.properties");
+        autoConfigureDataSource();
+    }
+
+    
+
+    public static class Common extends HeaderbuttonTestContainer {
+
+        public static final HeaderbuttonTestContainer.Common INSTANCE = new HeaderbuttonTestContainer.Common();
+
+        private static volatile boolean initialized;
+
+        private Common() {
+        }
+
+        @Override
+        public void beforeAll(ExtensionContext extensionContext) throws Exception {
+            if (!initialized) {
+                super.beforeAll(extensionContext);
+                initialized = true;
+            }
+            setupContext();
+        }
+        
+
+        @SuppressWarnings("RedundantThrows")
+        @Override
+        public void afterAll(ExtensionContext extensionContext) throws Exception {
+            cleanupContext();
+            // never stops - do not call super
+        }
+        
+    }
+}
